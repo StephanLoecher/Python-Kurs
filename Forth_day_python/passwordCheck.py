4c62ef230f6b8faf278bb3d0200af780a15dd262
StephanLoecher <stephan.loecher@skyered.de>
2025-01-23 14:44:49 +0100

    dokumentation und beispiel "wie finde ich lösungsansätze"

--- Forth_day_python/passwordCheck.py
+++ Forth_day_python/passwordCheck.py
@@ -9,12 +9,16 @@
 
 # Abfrage des Passworts im Terminal
 password = input("Passwort eingabe: ")
+
 #Deklaration minimum Länge
 minLength = 8
-#Deklaration bool variable ist Paswort minimum Länger
+
+#Deklaration bool variable ist Passwort minimum Länger
 isPasswordMinLength = False
+
 #Deklaration Ob das Passwort eine Zahl enthält
 containsNumber = False
+
 #Deklaration on das Passwort eine Sonderzahl enthält
 containsSpecialChar = False
 
@@ -28,15 +32,40 @@ for char in password:
     if char.isnumeric():
         containsNumber = True
         break
-    # Prüfung ob das eingegebene Passwort ein Sonderzeichen enthält
+
+# Prüfung ob das eingegebene Passwort ein Sonderzeichen enthält
 for char in password:
     print(char)
     if not char.isalnum():
         containsSpecialChar = True
         break
+
 # Prüfung ob alle Kriterien erfüllt sind
 
 if isPasswordMinLength and containsNumber and containsSpecialChar:
     print("Password is safe")
 else:
     print("Password is not safe")
+
+
+#Konzept "Wie gehe ich mit unbekannten Lösungswegen um"
+
+# Variabel benennung
+path = "./"
+fileName = "password.txt"
+
+# Öffnen eines Dateistreams
+datei = open((path + fileName),"a+")
+
+# In die Datei schreiben
+datei.write("\n" + password)
+
+# Ausgabe der Inhalte der Datei
+print(datei.read())
+
+# Dateistream schließen
+datei.close()
+
+# Andere Form des stream öffnen mit automatisches schließen
+with open("beispiel.txt", "a+") as datei:
+    datei.write(f"\n{password}")
