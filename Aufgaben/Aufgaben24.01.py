d9a1b648d8ac186691b6c32095889d1a70bc6e49
StephanLoecher <stephan.loecher@skyered.de>
2025-01-24 12:48:09 +0100

    Added definitions, added calss and inheritance, added aufgaben

---
+++ Aufgaben/Aufgaben24.01.py
@@ -0,0 +1,11 @@
+# Erstelle 8 Variabeln mit je einem anderen Datentyp
+# Benenne die Datentypen der Variabeln
+# Nehme einen Input entgegen und speichere sie in die Variabel ab
+# Checke den Typ und gebe den Typ der Inputvariabel in der console aus
+# Schreibe eine Methode
+# Nehme in der Methode 2 parameter entgegen.
+# Schreibe eine beliebige funktionierende Logik mit folgendem Inhalt:
+# - es muss ein if statement drin sein
+# - es muss eine for loop enthalten sein
+# - es muss sein logischer operator enthalten sein
+# führe die Methode aus
--- Forth_day_python/class_and_inhertiance.py
+++ Forth_day_python/class_and_inhertiance.py
@@ -16,8 +16,35 @@ class Student(Human):
     def walk(self, distance):
         return super().walk(distance)
 
+    
+
 x = Student(["c#", "python", "Ansible"], ["Web-Developement", "Dev-Ops"], 46, "Mathias", True,)
 
 x.walk(23)
 print(x.interrests)
 print(x.age)
+
+
+class Tier:
+    def sprich(self):
+        print("Das Tier macht ein Geräusch")
+ 
+# Abgeleitete Klasse 1
+class Hund(Tier):
+    def sprich(self):
+        print("Der Hund bellt: Wuff Wuff!")
+ 
+# Abgeleitete Klasse 2
+class Katze(Tier):
+    def sprich(self):
+        print("Die Katze miaut: Miau Miau!")
+ 
+# Hauptprogramm
+tier = Tier()
+tier.sprich()  # Gibt aus: Das Tier macht ein Geräusch
+ 
+hund = Hund()
+hund.sprich()  # Gibt aus: Der Hund bellt: Wuff Wuff!
+ 
+katze = Katze()
+katze.sprich()  # Gibt aus: Die Katze miaut: Miau Miau!
